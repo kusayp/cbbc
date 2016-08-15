3ef32f277795a418f6a2bbd1de4d9b4ecbcc314b
Joseph Tetteh <josephkusaytetteh@gmail.com>
2016-08-15 09:39:16 +0100

    initial

--- cbbc/settings.py
+++ cbbc/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aboutus',
+    'contact',
+    'events',
+    'home',
+    'ministries',
+    'resources',
+    'hymn',
 ]
 
 MIDDLEWARE_CLASSES = [
