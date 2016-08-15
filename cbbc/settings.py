202ed14c2d553b5b36430dbba657e4c2d50fabe4
Joseph Tetteh <josephkusaytetteh@gmail.com>
2016-08-15 09:45:06 +0100

    initial

--- cbbc/settings.py
+++ cbbc/settings.py
@@ -81,12 +81,18 @@ WSGI_APPLICATION = 'cbbc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES={}
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] =  dj_database_url.config()
+    DATABASES['default']['CONN_MAX_AGE'] =  960
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
@@ -126,3 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
