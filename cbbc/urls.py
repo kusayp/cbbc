6788a2d3b824927eec2c0b9ffd8b9eaa96947033
Joseph Tetteh <josephkusaytetteh@gmail.com>
2016-08-15 10:52:38 +0100

    initial

--- cbbc/urls.py
+++ cbbc/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     # url(r'^jet/', include('jet.urls', 'jet')),
     # url(r'^jet/dashboard', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
-    # url(r'^home/', include('home.urls')),
     url(r'^ministries/', include('ministries.urls')),
     url(r'^aboutus/', include('aboutus.urls')),
     url(r'^events/', include('events.urls')),
